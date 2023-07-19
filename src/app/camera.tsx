3f5e5c18e34a68e0b1c1d3105e7d6a69c96b23e7
Dex Mills <dmills@instructors.2u.com>
2023-07-19 13:54:45 -0600

    Build out of camera

---
+++ src/app/camera.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, {useRef, useState, useEffect} from'react'
+
+const Camera=()=>{
+
+    const videoRef=useRef<any>(null);
+    const [mediaStream, setMediaStream]= useState<MediaStream |null>(null);
+
+    const sendVideoData=(data:string)=>{
+        socket.emit();
+    }
+
+    useEffect(()=>{
+        const enableVideoStream = async()=>{
+            try{
+                const stream= await navigator.mediaDevices.getUserMedia({video: true});
+                setMediaStream(stream)
+            }catch(err){
+                console.error("Errror accessing web cam",err)
+            }
+        }
+        enableVideoStream()
+    },[])
+
+    useEffect(()=>{
+        if(videoRef.current && mediaStream){
+            videoRef.current.srcObject=mediaStream
+        }
+    },[videoRef,mediaStream])
+
+    useEffect(()=>{
+        return()=>{
+            if(mediaStream){
+                mediaStream.getTracks().forEach((track)=>{
+                    track.stop()
+                })
+            }
+        }
+    },[mediaStream])
+    return (
+        <div>
+            <video ref={videoRef} autoPlay={true}/>
+        </div>
+    )
+}
+
+export default Camera;
