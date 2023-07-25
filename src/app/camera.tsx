5307258380e10090ee0658202db02ec107c0fb47
You know who I am <dmills@extraspace.com>
2023-07-25 11:01:08 -0600

    Basics set up

--- src/app/camera.tsx
+++ src/app/camera.tsx
@@ -1,41 +1,79 @@
 'use client'
-import React, {useRef, useState, useEffect} from'react'
+import React, { useRef, useState, useEffect } from 'react'
+import * as tf from "@tensorflow/tfjs"
+import * as facemesh from "@tensorflow-models/facemesh"
+import '@tensorflow/tfjs-backend-cpu';
 
-const Camera=()=>{
 
-    const videoRef=useRef<any>(null);
-    const [mediaStream, setMediaStream]= useState<MediaStream |null>(null);
+const Camera = () => {
 
-    useEffect(()=>{
-        const enableVideoStream = async()=>{
-            try{
-                const stream= await navigator.mediaDevices.getUserMedia({video: true});
+    const videoRef = useRef<any>(null);
+    const canvasRef = useRef<any>(null)
+
+    const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+
+    useEffect(() => {
+        const enableVideoStream = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
                 setMediaStream(stream)
-            }catch(err){
-                console.error("Errror accessing web cam",err)
+            } catch (err) {
+                console.error("Errror accessing web cam", err)
             }
         }
         enableVideoStream()
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        if(videoRef.current && mediaStream){
-            videoRef.current.srcObject=mediaStream
+    useEffect(() => {
+        if (videoRef.current && mediaStream) {
+            videoRef.current.srcObject = mediaStream
         }
-    },[videoRef,mediaStream])
+    }, [videoRef, mediaStream])
 
-    useEffect(()=>{
-        return()=>{
-            if(mediaStream){
-                mediaStream.getTracks().forEach((track)=>{
+    useEffect(() => {
+        return () => {
+            if (mediaStream) {
+                mediaStream.getTracks().forEach((track) => {
                     track.stop()
                 })
             }
         }
-    },[mediaStream])
+    }, [mediaStream])
+
+    const runFaceMesh = async () => {
+        const net = await facemesh.load();
+
+        setInterval(() => {
+            detect(net)
+        }, 10000)
+    }
+
+    runFaceMesh();
+
+    const detect = async (net: facemesh.FaceMesh) => {
+        if (
+            videoRef.current.video != "undefined" &&
+            videoRef.current !== null
+        ) {
+            const video = videoRef.current.video;
+            const videoWidth = videoRef.current.clientwidth;
+            const videoHeight = videoRef.current.clientHeight;
+
+            canvasRef.current.width = videoWidth;
+            canvasRef.current.height = videoHeight;
+
+            const face = await net.estimateFaces(video);
+            console.log(face)
+        }
+    }
+
+
+
     return (
         <div>
-            <video ref={videoRef} autoPlay={true}/>
+            <video ref={videoRef} autoPlay={true}
+                style={{ width: 640, height: 480 }} />
+            <canvas ref={canvasRef} />
         </div>
     )
 }
