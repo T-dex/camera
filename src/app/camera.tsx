917276c53efefc19e40df28044ca733b8a0401f8
Dex Mills <dmills@instructors.2u.com>
2023-11-06 16:33:41 -0700

    Canvas branch with working datapoints

--- src/app/camera.tsx
+++ src/app/camera.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, MutableRefObject } from 'react';
 import '@mediapipe/face_detection';
 import '@tensorflow/tfjs-core';
 // Register WebGL backend.
@@ -7,23 +7,23 @@ import '@tensorflow/tfjs-backend-webgl';
 import * as faceDetection from '@tensorflow-models/face-detection';
 
 
-
-
-
 const Camera = () => {
     let detector;
 
     const videoRef = useRef<any>(null);
+    const c1 = useRef<HTMLCanvasElement>(null); 
+
 
 
     const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
     const [funInTheSun, setFunInTheSun] = useState<boolean>(false)
     const [faceData, setFaceData]= useState<any>()
-
+    
     useEffect(() => {
         const enableVideoStream = async () => {
             try {
                 const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                // videoRef.current.src=window.URL.createObjectURL(stream
                 setMediaStream(stream)
             } catch (err) {
                 console.error("Errror accessing web cam", err)
@@ -31,13 +31,14 @@ const Camera = () => {
         }
         enableVideoStream()
     }, [])
-
+    
     useEffect(() => {
         if (videoRef.current && mediaStream) {
             videoRef.current.srcObject = mediaStream
+            videoRef.current.play()
         }
     }, [videoRef, mediaStream])
-
+    
     useEffect(() => {
         return () => {
             if (mediaStream) {
@@ -47,8 +48,14 @@ const Camera = () => {
             }
         }
     }, [mediaStream])
-
-    const detect = async () => {
+    
+    const detect = async (context: CanvasRenderingContext2D | undefined) => {
+        if(!c1.current)return;
+        // paintToCanvas()
+        const paint= setInterval(()=>{
+            context?.drawImage(videoRef.current,0,0)
+            },1000)
+    
         if (
             typeof (videoRef.current != undefined) &&
             videoRef.current !== null &&
@@ -57,30 +64,42 @@ const Camera = () => {
             const video = videoRef.current;
             const videoWidth = videoRef.current.videoWidth;
             const videoHeight = videoRef.current.videoHeight;
+            
+            video.current=c1.current.captureStream(1)
 
+            c1.current.height=videoHeight;
+            c1.current.width=videoWidth;
+
+   
 
             const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
             const detectorConfig: faceDetection.MediaPipeFaceDetectorTfjsModelConfig = {
-                runtime: "tfjs"
+                runtime: "tfjs",
+                maxFaces: 1
             }
             detector = await faceDetection.createDetector(model, detectorConfig)
 
 
-            const faces = await detector.estimateFaces(video)
+            const faces = await detector.estimateFaces(c1.current)
+            console.log(faces)
             setFaceData(faces)
             faces.length == 1 ? setFunInTheSun(true) : setFunInTheSun(false)
-            console.log(faceData)
-            return
+            return ()=>{
+                clearInterval(paint)
+            }
         }
     }
+
+    
     useEffect(() => {
-        const check = setInterval(() => {
-            detect()
-        }, 1000)
+
+        const canvas = c1.current
+        const context= canvas?.getContext('2d')
+
+        const check = setInterval(() => detect(context), 1000)
         return () => {
             clearInterval(check);
         }
-
     }, [])
 
 
@@ -88,8 +107,12 @@ const Camera = () => {
     return (
         <div>
             <video ref={videoRef} autoPlay={true}
-                style={{ width: "480px", height: "640px"}} />
+                style={{ width: "480px", height: "640px", display:"none"}} muted/>
+            
             <div>
+                <canvas id="vidDisplay" ref={c1} style={ {display:"none"}}/>
+                </div>
+                <div>
                 {funInTheSun ? (<h1 style={{ color: "green" }}>Human Detected </h1>) : (<h1 style={{ color: 'red' }}>No Human Detected</h1>)}
             </div>
         </div>
